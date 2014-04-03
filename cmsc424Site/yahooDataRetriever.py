31596961df50c7b1aef298763fad538bd62b5209
zinglax <zinglax@github.com>
2014-04-02 23:27:03 -0400

    make changes to yahooDataRetriever so that data is collected for all s&p 500 companies and saved in csv files under /Data.  Still need to parse this information into a database. Once this is done the database should be saved and shouln't be touched, it will have all of the historial data ie data from 2008 to 2013 for all the companies we are worried about.

--- cmsc424Site/yahooDataRetriever.py
+++ cmsc424Site/yahooDataRetriever.py
@@ -1,7 +1,7 @@
 import ystockquote
+import requests
 
 ticker_file = "./SandP500TickerSymbols.txt"
-
 
 def get_top_500_tickers(f):
   ''' Generates a list of S&P 500 tickers and returns them '''
@@ -20,12 +20,12 @@ def get_top_500_tickers(f):
   return j
 
 
-
-def get_historical_csv(company_ticker, from_date, to_date):
-  ''' Gets historical csv file for a company in a certain time span. 
-  date formated as mmddyyyy
+def historical_url_generator(company_ticker, from_date, to_date):
+  ''' Creates a URL for the historical data of a given company, dates formatted
+  as mmddyyyy
   '''
   
+  # Parsing date strings
   from_month = int(from_date[0:2])
   from_day = int(from_date[2:4])
   from_year = int(from_date[4:8])
@@ -34,36 +34,34 @@ def get_historical_csv(company_ticker, from_date, to_date):
   to_day = int(to_date[2:4])
   to_year = int(to_date[4:8])
   
-
-
-  ## Stripping the leading 0's if their are any
-  #if from_month[0] == "0":
-    #from_month = from_month[1]
-  
-  #if from_day[0] == "0":
-    #from_day = from_day[1]
-    
-  #if to_month[0] == "0":
-    #to_month = to_month[1]
-  
-  #if to_day[0] == "0":
-    #to_day = to_day[1]  
-  
   # Decrementing the month to work with url
   from_month = from_month - 1
   to_month = to_month - 1
-  
-  print from_month
-  print from_day
-  print from_year
-  print "hello"
-  print to_date
-  print to_month
-  print to_year
 
-  url = "http://ichart.yahoo.com/table.csv?s="
+  # Formatting URL
+  url = "http://ichart.yahoo.com/table.csv?s=" + company_ticker + "&a=" +\
+    str(from_month) + "&b=" + str(from_day) + "&c=" + str(from_year) + "&d=" +\
+    str(to_month) + "&e=" + str(to_day) +"&f=" + str(to_year)
+  
+  return url
   
   
+def retrieve_csv_file(url, company_ticker, to_date, from_date):
+  " Retrieves the csv file for a given company over a period of time "
+  # Generates Filename
+  filename = company_ticker + "~" + to_date + "~" + from_date
   
+  r = requests.get(url, stream=True)
   
-get_historical_csv("GOOG", "01012008", "12312013")
+  # Writes data to file from requests stream
+  with open("./Data/"+filename, 'wb') as fd:
+      for chunk in r.iter_content(chunk_size=1024):
+          fd.write(chunk)
+  
+def get_S_and_P_500_data():
+  
+  tickers = get_top_500_tickers(ticker_file)
+
+  for t in tickers:
+    url = historical_url_generator(t, "01012008", "12312013")
+    retrieve_csv_file(url, t, "01012008", "12312013")
